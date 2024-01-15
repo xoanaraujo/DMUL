bb12e1ac134d99a4bca713d17d870102ccac5048
xoanaraujo <115078254+xoanaraujo@users.noreply.github.com>
2024-01-15 10:39:27 +0100

    ...

--- UD2/SpaceRescue/core/src/io/github/xoanaraujo/spacerescue/MyGdxGame.java
+++ UD2/SpaceRescue/core/src/io/github/xoanaraujo/spacerescue/MyGdxGame.java
@@ -3,6 +3,8 @@ package io.github.xoanaraujo.spacerescue;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
@@ -15,7 +17,9 @@ import java.awt.Rectangle;
 import java.awt.geom.Rectangle2D;
 import java.util.Random;
 
-public class MyGdxGame extends ApplicationAdapter {
+public class MyGdxGame extends ApplicationAdapter{
+	private static int WIDTH;
+	private static int HEIGHT;
 	private static final Random rd = new Random();
 	private static Color bgColor;
 	private static final int SIZE = 64;
@@ -24,58 +28,84 @@ public class MyGdxGame extends ApplicationAdapter {
 	private float time;
 	private float velocity;
 	private Vector2 direction, position;
-	private boolean bouncedX, bouncedY;
+	private boolean launched;
 
 	@Override
 	public void create () {
+		WIDTH = Gdx.graphics.getWidth();
+		HEIGHT = Gdx.graphics.getHeight();
+		InputMultiplexer inputMultiplexer = new InputMultiplexer();
+		inputMultiplexer.addProcessor(new PlanetInputAdapter());
+		inputMultiplexer.addProcessor(new InputAdapter());
+		Gdx.input.setInputProcessor(inputMultiplexer);
+
 		batch = new SpriteBatch();
 		bgColor = new Color(0.14f,0.14f,0.18f, 1);
 
 		earthTexture = new Texture(Gdx.files.internal("Terran.png"));
 		earthTexture.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
-		position = new Vector2(1, 1);
 		time = 0;
-		velocity = 1000f;
+		velocity = 1f;
+		launched = false;
 
-		direction = new Vector2( rd.nextFloat(0, 1), rd.nextFloat(0,1)); //
 	}
 
 	@Override
 	public void render() {
 		ScreenUtils.clear(bgColor);
-		float deltatime = Gdx.graphics.getDeltaTime();
-		time += deltatime;
-		if (time > 1){
-			time-= 1;
-			bgColor =new Color(rd.nextFloat(1), rd.nextFloat(1), rd.nextFloat(1), 1);
-		}
-		if (position.x >= (Gdx.graphics.getWidth() - earthTexture.getWidth())){
-			position.x = Gdx.graphics.getWidth() - earthTexture.getWidth();
-			direction.x = - direction.x;
-		}
-		if ((position.x <= 0)){
-			position.x = 0;
-			direction.x = - direction.x;
-		}
-		if (position.y >= (Gdx.graphics.getHeight() - earthTexture.getHeight())){
-			position.y = Gdx.graphics.getHeight() - earthTexture.getHeight();
-			direction.y = - direction.y;
-		}
-		if ((position.y <= 0)){
-			position.y = 0;
-			direction.y = - direction.y;
-		}
-		position.x = (position.x + velocity * direction.x * deltatime);
-		position.y = (position.y + velocity * direction.y * deltatime);
+		if (launched){
+			float deltatime = Gdx.graphics.getDeltaTime();
 
-		batch.begin();
-		batch.draw(earthTexture, position.x, position.y);
-		batch.end();
+			if(position.x >= WIDTH - earthTexture.getWidth()){
+				position.x = WIDTH - earthTexture.getWidth();
+				direction.x *= -1;
+			} else if (position.x <= 0 ){
+				position.x = 0;
+				direction.x *= -1;
+			}
+			if(position.y >= HEIGHT - earthTexture.getHeight()){
+				position.y = HEIGHT - earthTexture.getHeight();
+				direction.y *= -1;
+			} else if (position.y <= 0 ){
+				position.y = 0;
+				direction.y *= -1;
+			}
+
+
+			position.x 	= (position.x + velocity * direction.x * deltatime);
+			position.y = (position.y + velocity * direction.y * deltatime);
+
+			batch.begin();
+			batch.draw(earthTexture, position.x, position.y);
+			batch.end();
+		}
+
 	}
 
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
+	}
+	private class PlanetInputAdapter extends InputAdapter{
+		@Override
+		public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+			if (!launched){
+				position = new Vector2(screenX , HEIGHT - screenY );
+			}
+			return super.touchDown(screenX, screenY, pointer, button);
+		}
+
+		@Override
+		public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+			if (!launched){
+				direction = new Vector2(screenX - position.x, HEIGHT - screenY - position.y);
+				Gdx.app.log("Position", position.toString());
+				Gdx.app.log("Direction", direction.toString());
+				launched = true;
+			} else
+				launched = false;
+			return super.touchUp(screenX, screenY, pointer, button);
+		}
 	}
 }
